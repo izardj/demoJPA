4ec3be760955bbebcd7e5ba4f430d55e69e1c374
izardj <izard.jerome@free.fr>
2017-04-13 14:57:08 +0200

    Annotation de la classe metier

--- src/metier/Contact.java
+++ src/metier/Contact.java
@@ -1,7 +1,15 @@
 package metier;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Contact {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int id;
 	private String nom;
 	private String prenom;
